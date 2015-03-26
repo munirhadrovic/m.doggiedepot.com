74c0bfc724d60941f236aa296886ea07eba80e1e
novicaa <novicamiketic@gmail.com>
2015-03-26 10:29:10 +0100

    promjene footer logoo i strelice

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		
 		<link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
-
+        <title>::Welcome to Doggie Depot::</title>
 		<script src="js/jquery-1.11.2.js"></script>
 		<script>
 		    $(document).on("mobileinit", function(evt) {
@@ -169,7 +169,7 @@
 		    <h1><img src="img/logo.png" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-		    <h1>Mobile Site<a href="http://www.doggiedepot.com" rel="external">  Clasic Site</a></h1>
+            <h1>Powered by <a href="http://www.mobilegrows.com" rel="external">MobileGrows</a> | <a href="http://www.doggiedepot.com/" rel="external">Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
@@ -199,6 +199,7 @@
                             <img src="img/slider-images/slider5.jpg" alt="">
                             <!-- <p class="caption">The third caption</p> -->
                         </li>
+
 				    </ul>
 			    </div>
 			    <!-- Jssor Slider End -->
